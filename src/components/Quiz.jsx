4457270ab646676ab035d498f5f6339c6153e59a
Nilton <niltonroese@gmail.com>
2022-10-17 14:22:17 +1000

    main components added

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,12 +1,44 @@
-import React from "react";
-import Header from './Header';
-import Footer from './Footer';
+import React, { useEffect, useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
 
 function Quiz() {
+  const [quotes, setQuotes] = useState([]);
+
+  //fetch quotes
+  useEffect(() => {
+    fetch("http://localhost:3001/quotes")
+      .then((resp) => resp.json())
+      .then((json) => {
+        console.log(json);
+        setQuotes(json);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
   return (
     <div>
       <Header />
-      <h1>Quiz view</h1>
+      <div>
+        {quotes.map((q) => {
+          return (
+            <div key={q.id}>
+              <h6>{q.quote}</h6>
+              <p>{q.characterOptions}</p>
+            </div>
+          );
+        })}
+      </div>
+      <div>{quotes[0].quote}</div>
+      {/* <div>
+        {quotes[0].characterOptions.map((c) => (
+          <button>
+            {c.character}
+          </button>
+        ))}
+      </div> */}
       <Footer />
     </div>
   );
