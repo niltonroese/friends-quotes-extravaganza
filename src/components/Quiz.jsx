ab2f7182d59de347cbebe24fcde8fb2b872d48e3
Nilton <niltonroese@gmail.com>
2022-10-21 12:31:26 +1000

    quiz rendering error fix

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,48 +1,59 @@
-import React, { useEffect, useState } from "react"
-import axios from 'axios'
-import Header from "./Header"
-import Footer from "./Footer"
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Header from "./Header";
+import Footer from "./Footer";
 
 function Quiz() {
-  const [quotes, setQuotes] = useState([])
-  const [currentQuote, setCurrentQuote] = useState(0)
-//   const [showScore, setShowScore] = useState(false)
+  const [quotes, setQuotes] = useState([]);
+  const [currentQuote, setCurrentQuote] = useState(0);
+  const [showScore, setShowScore] = useState(false);
 
   //fetch quotes
   useEffect(() => {
-    axios.get('http://localhost:3001/quotes')
-      .then(res => {
-        console.log(res)
-        setQuotes(res.data)
+    axios
+      .get("http://localhost:3001/quotes")
+      .then((res) => {
+        console.log(res);
+        setQuotes(res.data);
       })
-      .catch(err => {
-        console.log(err.message)
-      })
+      .catch((err) => {
+        console.log(err.message);
+      });
   }, []);
 
   const handleAnswerButtonClick = () => {
     const nextQuote = currentQuote + 1;
     if (nextQuote < quotes.length) {
-      setCurrentQuote(nextQuote)
+      setCurrentQuote(nextQuote);
     } else {
-      alert("end of quiz")
+      setShowScore(true);
     }
   };
 
   return (
     <div>
       <Header />
-      <div className="container-fluid text-center">
-        <span>Quote 1/{quotes.length}</span>
-        <div>{quotes[currentQuote].quote}</div>
-        <div>
-          {quotes[currentQuote].answers.map((a) => (
-            <button onClick={handleAnswerButtonClick} key={a.id}>
-              {a}
-            </button>
-          ))}
+      {showScore ? (
+        <div className="container-fluid text-center">
+          You've nailed 1 out of {quotes.length} quotes
         </div>
-      </div>
+      ) : (
+        <>
+          {quotes?.length && (
+            <div className="container-fluid text-center">
+              <span>Quote 1/{quotes.length}</span>
+              <div>{quotes[currentQuote].quote}</div>
+              <div>
+                {quotes[currentQuote].answers.map((a) => (
+                  <button onClick={handleAnswerButtonClick} key={a.id}>
+                    {a}
+                  </button>
+                ))}
+              </div>
+            </div>
+          )}
+        </>
+      )}
       <Footer />
     </div>
   );
