60b08b5fb6579128e8c0ca5f675a089e3a988b6f
Nilton <niltonroese@gmail.com>
2022-10-30 19:48:38 +1000

    character basic functionality added

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,12 +1,46 @@
-import React from "react";
-import Header from './Header';
-import Footer from './Footer';
+import React, { useEffect, useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import ReactCardFlip from "react-card-flip";
+import Chandler_car from "../images/Chandler_car.png";
+import axios from 'axios'
 
 function Characters() {
+  const [flip, setFlip] = useState(false);
+  const [bio, setBio] = useState([])
+
+  const getBio = async () => {
+    try {
+        const res = await axios.get("http://localhost:3001/characters/3");
+        console.log(res);
+        setBio(res.data);
+    } catch (err) {
+        alert(err.message);
+    }
+  }
+
+  useEffect (() => {
+    getBio ();
+  }, []);
+
   return (
     <div>
       <Header />
-      <h1>Characters view</h1>
+      <ReactCardFlip isFlipped={flip} flipDirection="horizontal">
+          <div>
+            <img
+              onClick={() => setFlip(!flip)}
+              alt="caricature"
+              src={Chandler_car}
+            />
+          </div>
+          <div onClick={() => setFlip(!flip)}>
+            <div>Name: {bio.name}</div>
+            <div>Date of Birth: {bio.date_of_birth}</div>
+            <div>Occupation: {bio.occupation}</div>
+            <div>Traits: {bio.traits}</div>
+        </div>
+      </ReactCardFlip>
       <Footer />
     </div>
   );
