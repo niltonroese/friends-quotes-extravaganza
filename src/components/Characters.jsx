bab0580a43153fa2ccbcf4cea964b70e697d4b34
Nilton <niltonroese@gmail.com>
2022-11-01 18:27:49 +1000

    fliping hover functionality added

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,46 +1,154 @@
 import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import ReactCardFlip from "react-card-flip";
 import Chandler_car from "../images/Chandler_car.png";
-import axios from 'axios'
+import Monica_car from "../images/Monica_car.png";
+import Joey_car from "../images/Joey_car.png";
+import Ross_car from "../images/Ross_car.png";
+import Rachel_car from "../images/Rachel_car.png";
+import Phebes_car from "../images/Phebes_car.png";
+import axios from "axios";
+import "./Characters.css";
 
 function Characters() {
-  const [flip, setFlip] = useState(false);
-  const [bio, setBio] = useState([])
+  const [bio, setBio] = useState([]);
 
   const getBio = async () => {
     try {
-        const res = await axios.get("http://localhost:3001/characters/3");
-        console.log(res);
-        setBio(res.data);
+      const res = await axios.get("http://localhost:3001/characters/3");
+      console.log(res);
+      setBio(res.data);
     } catch (err) {
-        alert(err.message);
+      alert(err.message);
     }
-  }
+  };
 
-  useEffect (() => {
-    getBio ();
+  useEffect(() => {
+    getBio();
   }, []);
 
   return (
     <div>
       <Header />
-      <ReactCardFlip isFlipped={flip} flipDirection="horizontal">
-          <div>
-            <img
-              onClick={() => setFlip(!flip)}
-              alt="caricature"
-              src={Chandler_car}
-            />
-          </div>
-          <div onClick={() => setFlip(!flip)}>
-            <div>Name: {bio.name}</div>
-            <div>Date of Birth: {bio.date_of_birth}</div>
-            <div>Occupation: {bio.occupation}</div>
-            <div>Traits: {bio.traits}</div>
+      <div className="jumbotron jumbotron-fluid text-center mt-3">
+        <div className="container">
+          <h1 className="display-5">Main Characters</h1>
+          <p className="lead">Check out our beloved friends bios!</p>
         </div>
-      </ReactCardFlip>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-4 cont">
+            <div className="card text-center">
+              <div className="card_front">
+                <img src={Chandler_car} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h4 className="card-title">Chandler</h4>
+                  {/* <button className="btn btn-primary">
+                  Flip
+                </button> */}
+                </div>
+              </div>
+              <div className="card_back">
+                <div className="mt-5">
+                  <h4>{bio.name}</h4>
+                  <p>Date of Birth: {bio.date_of_birth}</p>
+                  <p>Occupation: {bio.occupation}</p>
+                  <p>Traits: {bio.traits}</p>
+                  <br />
+                  <br />
+                  <br />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-4">
+            <div className="card text-center">
+              <img src={Monica_car} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h4 className="card-title">Monica</h4>
+                {/* <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p> */}
+                <button href="#" className="btn btn-primary">
+                  Flip
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-4">
+            <div className="card text-center">
+              <img src={Joey_car} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h4 className="card-title">Joey</h4>
+                {/* <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p> */}
+                <button href="#" className="btn btn-primary">
+                  Flip
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container mt-4">
+        <div className="row">
+          <div className="col-4">
+            <div className="card text-center">
+              <img src={Phebes_car} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h4 className="card-title">Phoebe</h4>
+                {/* <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p> */}
+                <button href="#" className="btn btn-primary">
+                  Flip
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-4">
+            <div className="card text-center">
+              <img src={Ross_car} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h4 className="card-title">Ross</h4>
+                {/* <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p> */}
+                <button href="#" className="btn btn-primary">
+                  Flip
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-4">
+            <div className="card text-center">
+              <img src={Rachel_car} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h4 className="card-title">Rachel</h4>
+                {/* <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p> */}
+                <button href="#" className="btn btn-primary">
+                  Flip
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -20,7 +20,6 @@ function useQuiz() {
         console.log(err.message);
       });
   };
-
   return [quotes, fetchQuotes];
 }
 
@@ -32,7 +31,7 @@ function Quiz() {
 
   //fetch quotes
   useEffect(() => {
-    fetchQuotes()
+    fetchQuotes();
   }, []);
 
   const handleAnswerButtonClick = (value, answer) => {
@@ -51,75 +50,148 @@ function Quiz() {
     await fetchQuotes();
     setTimeout(() => setShowScore(false), 500);
     setCurrentQuote(0);
-    setScore(() => setShowScore(0), 500)
+    setTimeout(() => setScore(0), 500);
   };
 
   return (
     <div className="text-center">
       <Header />
-      <br />
-      {showScore ? (
-        <div className="container-fluid text-center">
-          <h4>Quiz Complete</h4>
-          <p>
-            You've nailed {score} out of {quotes.length} quotes
-          </p>
-          <button
-            type="button"
-            className="btn btn-info mb-3"
-            onClick={onRestartButtonClick}
-          >
-            Give it another Go!
-          </button>
-          <p>Or</p>
-          <Link className="link-primary" to="/FavoriteQuote">
-            Help us to increase our quotes database, add one here!
-          </Link>
-          <Outlet />
+      <div className="jumbotron jumbotron-fluid text-center mt-3">
+        <div className="container">
+          <h1 className="display-5">Quiz me!</h1>
         </div>
-      ) : (
-        <>
-          {quotes?.length && (
-            <div className="container-fluid text-center">
-              <h4>
-                Quote {currentQuote + 1}/{quotes.length}
-              </h4>
-              <div>{quotes[currentQuote].quote}</div>
-              <br />
-              <div>
-                <button type="button"
-                    className="btn btn-info" value="Chandler" onClick={(e) =>
-                        handleAnswerButtonClick(e.target.value, quotes[currentQuote].answer)}>Chandler</button>
-                <button type="button"
-                    className="btn btn-info" value="Monica" onClick={(e) =>
-                        handleAnswerButtonClick(e.target.value, quotes[currentQuote].answer)}>Monica</button>
-                <button type="button"
-                    className="btn btn-info" value="Joey" onClick={(e) =>
-                        handleAnswerButtonClick(e.target.value, quotes[currentQuote].answer)}>Joey</button>
-                <button type="button"
-                    className="btn btn-info" value="Phoebe" onClick={(e) =>
-                        handleAnswerButtonClick(e.target.value, quotes[currentQuote].answer)}>Phoebe</button>
-                <button type="button"
-                    className="btn btn-info" value="Ross" onClick={(e) =>
-                        handleAnswerButtonClick(e.target.value, quotes[currentQuote].answer)}>Ross</button>
-                <button type="button"
-                    className="btn btn-info" value="Rachel" onClick={(e) =>
-                        handleAnswerButtonClick(e.target.value, quotes[currentQuote].answer)}>Rachel</button>
-              </div>
-            </div>
-          )}
-        </>
-      )}
-      <div>
-        <br />
-        <img
-          src={Friends_colorful}
-          className="img-fluid"
-          alt="Author"
-          height={681}
-          width={523}
-        />
       </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            {/* <div className="card"> */}
+            {showScore ? (
+              <div className="container-fluid text-center">
+                <h4>Quiz Complete</h4>
+                <p>
+                  You've nailed {score} out of {quotes.length} quotes
+                </p>
+                <button
+                  type="button"
+                  className="btn btn-info mb-3"
+                  onClick={onRestartButtonClick}
+                >
+                  Give it another Go!
+                </button>
+                <p>Or</p>
+                <Link className="link-primary" to="/FavoriteQuote">
+                  Help us to increase our quotes database, add one here!
+                </Link>
+                <Outlet />
+              </div>
+            ) : (
+              <>
+                {quotes?.length && (
+                  <div className="container-fluid text-center">
+                    <h4>
+                      Quote {currentQuote + 1}/{quotes.length}
+                    </h4>
+                    <div>{quotes[currentQuote].quote}</div>
+                    <br />
+                    <div>
+                      <button
+                        type="button"
+                        className="btn btn-info"
+                        value="Chandler"
+                        onClick={(e) =>
+                          handleAnswerButtonClick(
+                            e.target.value,
+                            quotes[currentQuote].answer
+                          )
+                        }
+                      >
+                        Chandler
+                      </button>
+                      <button
+                        type="button"
+                        className="btn btn-info"
+                        value="Monica"
+                        onClick={(e) =>
+                          handleAnswerButtonClick(
+                            e.target.value,
+                            quotes[currentQuote].answer
+                          )
+                        }
+                      >
+                        Monica
+                      </button>
+                      <button
+                        type="button"
+                        className="btn btn-info"
+                        value="Joey"
+                        onClick={(e) =>
+                          handleAnswerButtonClick(
+                            e.target.value,
+                            quotes[currentQuote].answer
+                          )
+                        }
+                      >
+                        Joey
+                      </button>
+                      <button
+                        type="button"
+                        className="btn btn-info"
+                        value="Phoebe"
+                        onClick={(e) =>
+                          handleAnswerButtonClick(
+                            e.target.value,
+                            quotes[currentQuote].answer
+                          )
+                        }
+                      >
+                        Phoebe
+                      </button>
+                      <button
+                        type="button"
+                        className="btn btn-info"
+                        value="Ross"
+                        onClick={(e) =>
+                          handleAnswerButtonClick(
+                            e.target.value,
+                            quotes[currentQuote].answer
+                          )
+                        }
+                      >
+                        Ross
+                      </button>
+                      <button
+                        type="button"
+                        className="btn btn-info"
+                        value="Rachel"
+                        onClick={(e) =>
+                          handleAnswerButtonClick(
+                            e.target.value,
+                            quotes[currentQuote].answer
+                          )
+                        }
+                      >
+                        Rachel
+                      </button>
+                    </div>
+                  </div>
+                )}
+              </>
+            )}
+            <div>
+              <br />
+              <img
+                src={Friends_colorful}
+                className="img-fluid"
+                alt="Author"
+                height={681}
+                width={523}
+              />
+            </div>
+            {/* </div> */}
+          </div>
+        </div>
+      </div>
+
       <Footer />
     </div>
   );
@@ -129,7 +201,7 @@ export default Quiz;
 
 // function useQuiz() {
 //     const [quotes, setQuotes] = useState([]);
-  
+
 //     const fetchQuotes = () => {
 //       axios
 //         .get("http://localhost:3001/quotes")
@@ -141,21 +213,21 @@ export default Quiz;
 //           console.log(err.message);
 //         });
 //     };
-  
+
 //     return [quotes, fetchQuotes];
 //   }
-  
+
 //   function Quiz() {
 //     const [quotes, fetchQuotes] = useQuiz();
 //     const [currentQuote, setCurrentQuote] = useState(0);
 //     const [showScore, setShowScore] = useState(false);
 //     const [score, setScore] = useState(0);
-  
+
 //     //fetch quotes
 //     useEffect(() => {
 //       fetchQuotes()
 //     }, []);
-  
+
 //     const handleAnswerButtonClick = (answerIndex, correctAnswerIndex) => {
 //       if (answerIndex === correctAnswerIndex) {
 //         setScore((curr) => curr + 1);
@@ -167,13 +239,13 @@ export default Quiz;
 //         setShowScore(true);
 //       }
 //     };
-  
+
 //     const onRestartButtonClick = async () => {
 //       await fetchQuotes();
 //       setTimeout(() => setShowScore(false), 500);
 //       setCurrentQuote(0);
 //     };
-  
+
 //     return (
 //       <div className="text-center">
 //         <Header />
@@ -241,5 +313,5 @@ export default Quiz;
 //       </div>
 //     );
 //   }
-  
+
 //   export default Quiz;
